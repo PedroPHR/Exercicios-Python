b12c05871462470eb827063b3ea8270b4d29b036
PedroPHR <pedroribeiro5500@gmail.com>
2023-01-25 10:50:48 -0300

    exercicio 4

---
+++ exercicios4.py
@@ -0,0 +1,13 @@
+print('Digites as quatro notas do aluno')
+
+nota1 = int(input('Nota 1: '))
+nota2 = int(input('Nota 2: '))
+nota3 = int(input('Nota 3: '))
+nota4 = int(input('Nota 4: '))
+
+
+media = nota1 + nota2 + nota3 + nota4
+
+media1 = media / 4
+
+print(f'Média do aluno: {media1}')
