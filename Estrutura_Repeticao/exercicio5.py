255a33492a2ae991b2b399a319633999506f0725
PedroPHR <pedroribeiro5500@gmail.com>
2023-02-11 02:15:00 -0300

    Novos

--- Estrutura_Repeticao/exercicio5.py
+++ Estrutura_Repeticao/exercicio5.py
@@ -2,15 +2,17 @@ print('PROGRAMA PARA CALCULAR TEMPO DE CRESCIMENTO DE UM PAIS PERANTE OUTRO')
 
 paisA = float(input('Digite a população do Pais A: '))
 paisB = float(input('Digite a população do Pais B: '))
+taxaA = float(input('Digita a taxa de crescimento do pais A: '))
+taxaB = float(input('Digita a taxa de crescimento do pais B: '))
 cont = 0
 
 if (paisA >= paisB):
     print('Impossivel fazer o calculo já que o pais A não deve ser mais que o pais B')
 else:
     while (paisA < paisB):
-        crescimentoA = paisA * 0.03
+        crescimentoA = paisA * taxaA
         paisA += crescimentoA
-        crescimentoB = paisB * 0.015
+        crescimentoB = paisB * taxaB
         paisB += crescimentoB
         cont += 1
         # print(paisA, paisB)
