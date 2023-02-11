a3d3d8ec224771897ceef1421ab792b21873f4d4
PedroPHR <pedroribeiro5500@gmail.com>
2023-02-11 02:10:41 -0300

    Editando

--- Estrutura_Repeticao/exercicio5.py
+++ Estrutura_Repeticao/exercicio5.py
@@ -4,13 +4,16 @@ paisA = float(input('Digite a população do Pais A: '))
 paisB = float(input('Digite a população do Pais B: '))
 cont = 0
 
-while (paisA < paisB):
-    crescimentoA = paisA * 0.03
-    paisA += crescimentoA
-    crescimentoB = paisB * 0.015
-    paisB += crescimentoB
-    cont += 1
-    print(paisA, paisB)
+if (paisA >= paisB):
+    print('Impossivel fazer o calculo já que o pais A não deve ser mais que o pais B')
 else:
-    print(
-        f'Quantidade de anos que demorou para o Pais A passar em população do Pais B foi: {cont} Anos')
+    while (paisA < paisB):
+        crescimentoA = paisA * 0.03
+        paisA += crescimentoA
+        crescimentoB = paisB * 0.015
+        paisB += crescimentoB
+        cont += 1
+        # print(paisA, paisB)
+    else:
+        print(
+            f'Quantidade de anos que demorou para o Pais A passar em população do Pais B foi: {cont} Anos')
